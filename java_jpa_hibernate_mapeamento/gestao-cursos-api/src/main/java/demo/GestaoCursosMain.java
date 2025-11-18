d653bcfd97c2d37e47e43f5acf3017379492230d
KarinaMCorreia <ka.mendes.correia@gmail.com>
2025-11-17 23:32:01 -0300

    feat: implementar projeto completo de gestão de cursos com JPA, Hibernate e SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -13,7 +13,6 @@ public class GestaoCursosMain {
         AlunoModel alunoModel = new AlunoModel();
         CursoModel cursoModel = new CursoModel();
 
-        // 1) Criar um aluno com 1 endereço e 1 telefone
         Aluno aluno = new Aluno();
         aluno.setNome("João da Silva");
         aluno.setEmail("joao.silva@example.com");
@@ -31,16 +30,13 @@ public class GestaoCursosMain {
 
         alunoModel.create(aluno);
 
-        // 2) Criar um professor
         Professor professor = new Professor();
         professor.setNome("Maria Oliveira");
         professor.setEmail("maria.oliveira@example.com");
 
-        // 3) Criar um material de curso
         MaterialCurso materialCurso = new MaterialCurso();
         materialCurso.setUrlMaterial("https://meusmateriais.com/curso-java-basico");
 
-        // 4) Criar um curso com professor, material e aluno
         Curso curso = new Curso();
         curso.setNome("Java Básico");
         curso.setProfessor(professor);
@@ -49,11 +45,9 @@ public class GestaoCursosMain {
 
         cursoModel.create(curso);
 
-        // 5) Buscar todos os alunos
         List<Aluno> alunos = alunoModel.findAll();
         System.out.println("Qtde de alunos encontrados : " + alunos.size());
 
-        // 6) Buscar todos os cursos
         List<Curso> cursos = cursoModel.findAll();
         System.out.println("Qtde de cursos encontrados : " + cursos.size());
 
